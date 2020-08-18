64ff7733f9a9d14bee53fa9fcfd01e6d88b42ea3
ghelanisg <69872826+ghelanisg@users.noreply.github.com>
2020-08-18 17:00:41 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************************************************************************
+ *Hello
+ **********************************************************************************
+ *Smeet Ghelani
+ *08/17/2020
+ *CMSC 255 C90
+ **********************************************************************************/
+ 
+ public class Hello
+ {
+	public static void main(String[] args)
+	{
+		System.out.println("Smeet Ghelani");
+		System.out.println("Hello");
+		System.out.println("The goal of this lab is to first make sure that you have Java and all related applications installed and ready to go. Secondly, the goal is for you to make sure that you can run your Java program.");
+		System.out.println("8/17/2020");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+	}
+}	
